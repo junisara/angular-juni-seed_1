13215e9367d57f40db385022633fa5672b436a56
git <junisara@gmail.com>
2015-01-02 16:32:02 +0900

    font-awesome 추가(오류수정)

--- Gruntfile.js
+++ Gruntfile.js
@@ -326,6 +326,11 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          cwd: 'bower_components/font-awesome/fonts',
+          dest: '<%= yeoman.dist %>/fonts',
+          src: '*'
         }]
       },
       styles: {
