eb2be4a51a420229253b94f8270b03fd4d65d91e
git <junisara@gmail.com>
2015-01-02 13:55:18 +0900

    파일추가

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modupetMoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the modupetMoApp
+ */
+angular.module('modupetMoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modupetMoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modupetMoApp
+ */
+angular.module('modupetMoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
